414190a5addd6177c03342eaa4fe8ef0b66faf8d
Martin Ewing <martin@aa6e.net>
2014-05-18 18:39:55 -0400

    shell scripts

--- try.sh
+++ try.sh
@@ -2,7 +2,11 @@
 
 # Audio test, Raspberry Pi, iMic soundcard, USB 1.1 ~85% cpu load
 # Use 'nice -20 ...' when running at highest CPU utilization.
-python iq.py --rate=48000 --size=384 --index=1 --skip=-1 --n_buffers=6 --WATERFALL --sp_min=-90 --sp_max=0 --v_min=-90 --v_max=0
+#python iq.py --rate=48000 --size=384 --index=1 --skip=-1 --n_buffers=6 --WATERFALL --sp_min=-90 --sp_max=0 --v_min=-90 --v_max=0
 
 # RTL Test, Raspberry Pi
 #python iq.py --RTL --WATERFALL --rtl_gain=0 --n_buffers=12 --size=384 --REV  
+
+# Audio test, BBB, iMic, USB 2.0 ~75% cpu load
+/home/martin/iq/iq.py --index=-1 --size=256 --n_buffers=6 --WATERFALL 
+
